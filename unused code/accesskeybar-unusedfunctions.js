811886a422a82746c003daa2b0ff0ef1fedaf0e7
Dale Price <dale@dalepricewebdesign.com>
2013-02-26 20:05:56 -0600

    remove "unused code" folder from repo

--- unused code/accesskeybar-unusedfunctions.js
+++
@@ -1,7 +0,0 @@
-
-function clickAccessKey(key)	//sends a message to the injected script to click the element with this key
-{
-	key = key.charAt(key.length-1);
-	
-	safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("clickAccessKey",key);
-}
